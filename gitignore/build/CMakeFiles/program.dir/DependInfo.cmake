2ac3a9b7e8f6eb122cab7f3621df351a12618f54
noam <noampz12@gmail.com>
2025-08-21 14:29:49 +0300

    making the repository look nice

---
+++ gitignore/build/CMakeFiles/program.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/C++/bank/account.cpp" "CMakeFiles/program.dir/account.cpp.obj" "gcc" "CMakeFiles/program.dir/account.cpp.obj.d"
+  "D:/C++/bank/app.cpp" "CMakeFiles/program.dir/app.cpp.obj" "gcc" "CMakeFiles/program.dir/app.cpp.obj.d"
+  "D:/C++/bank/bank.cpp" "CMakeFiles/program.dir/bank.cpp.obj" "gcc" "CMakeFiles/program.dir/bank.cpp.obj.d"
+  "D:/C++/bank/checkingAccount.cpp" "CMakeFiles/program.dir/checkingAccount.cpp.obj" "gcc" "CMakeFiles/program.dir/checkingAccount.cpp.obj.d"
+  "D:/C++/bank/customer.cpp" "CMakeFiles/program.dir/customer.cpp.obj" "gcc" "CMakeFiles/program.dir/customer.cpp.obj.d"
+  "D:/C++/bank/loan.cpp" "CMakeFiles/program.dir/loan.cpp.obj" "gcc" "CMakeFiles/program.dir/loan.cpp.obj.d"
+  "D:/C++/bank/savingsAccount.cpp" "CMakeFiles/program.dir/savingsAccount.cpp.obj" "gcc" "CMakeFiles/program.dir/savingsAccount.cpp.obj.d"
+  "D:/C++/bank/time.cpp" "CMakeFiles/program.dir/time.cpp.obj" "gcc" "CMakeFiles/program.dir/time.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
